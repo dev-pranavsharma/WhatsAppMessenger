419ea0d7894248718540efbc7a54cce7fa50dadb
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2025-07-13 14:08:16 +0530

    fb controller update

--- backend/routes/facebook.routes.js
+++ backend/routes/facebook.routes.js
@@ -1,9 +1,8 @@
 // routes/facebook.js
 import express from 'express'
-import { FBCodeExchange,FBCallback } from '../controllers/facebook.controller.js';
+import { FBCodeExchange } from '../controllers/facebook.controller.js';
 
 const router = express.Router();
 
-router.get('/callback',FBCallback);
 router.post('/exchange',FBCodeExchange)
 export default router;
