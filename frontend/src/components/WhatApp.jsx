f12312e57b0195c6c645eb06302fd17b2d007417
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2025-07-19 23:26:22 +0530

    added client data

--- frontend/src/components/WhatApp.jsx
+++ frontend/src/components/WhatApp.jsx
@@ -188,6 +188,7 @@ const WhatsAppSignupPopup = ({ prefill = {} }) => {
         try {
           const backendResponse = await sendToBackend({
             code,
+            ...clientData,
             ...prefill
           });
 
